45904ee35d0bca0c20be26696ba6d7c8c6e984fd
Fagner Moraes <fagm.carvalho@gmail.com>
2022-09-13 21:59:05 -0300

    criando controller

--- Controllers/AlunosController.cs
+++ Controllers/AlunosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AlunosApi.Service;
+using AlunosApi.Models;
 
 namespace AlunosApi.Controllers
 {
@@ -16,6 +17,18 @@ namespace AlunosApi.Controllers
         public AlunosController(IAlunoService alunoService)
         {
             _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos(){
+           try{
+                var alunos = await _alunoService.ObterTodosAlunos();
+                return Ok(alunos);
+           }
+           catch{
+                //return BadRequest();
+                return StatusCode(StatusCodes.Status500InternalServerError,"Erro ao obter Alunos");
+           } 
         }
 
 
